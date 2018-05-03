ebf591c2d6cd5e991d8adb2957f8fd93488d3a3c
wKich <nflazzer@gmail.com>
2018-05-03 23:06:31 +0500

    Fix sslcert path

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const express = require('express')
 const app = express()
 
 const options = {
-  cert: fs.readFileSync(path.join(__dirname, 'sslcert', 'fullchain.pem')),
-  key: fs.readFileSync(path.join(__dirname, 'sslcert', 'privkey.pem'))
+  cert: fs.readFileSync(path.join(__dirname, '..', 'sslcert', 'fullchain.pem')),
+  key: fs.readFileSync(path.join(__dirname, '..', 'sslcert', 'privkey.pem'))
 }
 
 app.use(express.static(path.join(__dirname, 'public')))
