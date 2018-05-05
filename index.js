1e8be41290900a7004b26e3b0ab9a5c96ea32e55
wKich <nflazzer@gmail.com>
2018-05-05 19:17:31 +0500

    Add `morgan` HTTP request logger middleware

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const fs = require("fs");
 const path = require("path");
 const dotenv = require("dotenv");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 dotenv.config();
 
+app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000);
