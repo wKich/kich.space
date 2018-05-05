468646ae48a378e69acfcec999c7fc3d1283d699
wKich <nflazzer@gmail.com>
2018-05-05 21:43:27 +0500

    Use typescript and add build step

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+const dotenv = require('dotenv')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+dotenv.config()
+
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000)
+
+if (process.env.NODE_ENV === 'production') {
+  const options = {
+    cert: fs.readFileSync(`${process.env.SSL_CERT_PATH}/fullchain.pem`),
+    key: fs.readFileSync(`${process.env.SSL_CERT_PATH}/privkey.pem`),
+  }
+
+  https.createServer(options, app).listen(3443)
+}
