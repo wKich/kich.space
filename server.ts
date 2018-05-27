32a7ac4fea8ca8917f1f57fb137baa7b3c428fc7
wKich <nflazzer@gmail.com>
2018-05-27 20:56:51 +0500

    Prepare server to hosting SPA

--- server.ts
+++ server.ts
@@ -10,7 +10,9 @@ const app = express()
 dotenv.config()
 
 app.use(morgan('combined'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api', (_req, res) => res.sendStatus(501))
+app.use('/static', express.static(path.join(__dirname, '/public')))
+app.get('*', (_req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 
 app.listen(3000)
 
